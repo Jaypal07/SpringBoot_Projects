47e1fd1fc3c5d5d89894ac05036644ec39fc6120
Jaypal Koli <kolijaypal47@gmail.com>
2025-07-07 16:12:44 +0530

    Project Basic Setup for calculator

---
+++ SpringBootWeb1/src/main/java/com/jaypal/HomeController.java
@@ -0,0 +1,34 @@
+package com.jaypal;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index.jsp";
+    }
+
+//    @RequestMapping("add")
+//    public String add(HttpServletRequest request, HttpSession session){ //servlet way of handling data
+//        int num1 = Integer.parseInt(request.getParameter("num1"));
+//        int num2 = Integer.parseInt(request.getParameter("num2"));
+//        int result = num1 + num2;
+//        session.setAttribute("result", result);
+//        System.out.println(result);
+//        return "result.jsp";
+//    }
+
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session){ //servlet way of handling data
+        int result = num1 + num2;
+        session.setAttribute("result", result);
+        System.out.println(result);
+        return "result.jsp";
+    }
+}
